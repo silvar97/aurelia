7ff57c19f62c1d74eda439e7735ec1007c1b73c1
silvar97 <yakosilvar@yahoo.de>
2020-09-29 13:39:58 +0200

    init

---
+++ src/test/java/com/discord/aurelia/AureliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discord.aurelia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
