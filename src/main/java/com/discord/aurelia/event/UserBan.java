23df106fe6dff9d45c495545a9293b79bbd297db
Columbro <ch-rzyska@t-online.de>
2020-10-19 21:24:56 +0200

    Test

--- src/main/java/com/discord/aurelia/event/UserBan.java
+++ src/main/java/com/discord/aurelia/event/UserBan.java
@@ -5,15 +5,20 @@ import java.util.Optional;
 import org.springframework.core.GenericTypeResolver;
 import org.springframework.stereotype.Component;
 
+import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.Event;
 
 import discord4j.core.event.domain.guild.BanEvent;
 import discord4j.core.object.Ban;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.User;
 
 
 
 @Component
 public class UserBan <T extends Event> implements EventListenerInterface<T> {
+
+    
 
     @Override
     @SuppressWarnings("unchecked")
@@ -27,7 +32,7 @@ public class UserBan <T extends Event> implements EventListenerInterface<T> {
         hookOnEvent(event);
 
     }
-
+    
     private final void hookOnEvent(Event event) {
        // if (event instanceof UserUpdateEvent){
        //    onUserUpdate((UserUpdateEvent) event); //Überprüft auf UserUpdateEvent
