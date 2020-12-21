b0648167aff7d5811387ec7e0164ac23d0ff9fd7
silvar97 <yakosilvar@yahoo.de>
2020-12-22 00:11:16 +0100

    regex fix

--- src/main/java/com/discord/aurelia/command/WarningCommand.java
+++ src/main/java/com/discord/aurelia/command/WarningCommand.java
@@ -1,6 +1,9 @@
 package com.discord.aurelia.command;
 
 import discord4j.core.event.domain.Event;
+import discord4j.rest.util.Color;
+import discord4j.rest.util.Permission;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +12,7 @@ public class WarningCommand implements CommandInterface {
     @Override
     public void execute(Event event) {
         // TODO Auto-generated method stub
-
+      Permission p = new Permissions();
     }
 
     @Override
--- src/main/java/com/discord/aurelia/constant/CommandConstant.java
+++ src/main/java/com/discord/aurelia/constant/CommandConstant.java
@@ -5,6 +5,8 @@ public class CommandConstant {
   public static final  String GENERAL_REGEX="(^[^0-9A-Za-z])([a-z]+)(?:[^a-zA-Z])?(?:[a-zA-Z]+)?(?: )?(?:<\\B@![0-9]+\\b>)?(?:[^0-9a-zA-Z][a-zA-Z]+[^a-zA-Z0-9][a-zA-Z]+)?(?:[^0-9a-zA-Z][a-zA-Z][a-zA-Z]+)?";
   public static final  String MENTIONLESS_REGEX="(^[^0-9A-Za-z])([a-z]+)(?:[^a-zA-Z])?(?:[a-zA-Z]+)?";
   public static final  String USER_METION_REGEX="(^[^0-9A-Za-z])([a-z]+)(?:[^a-zA-Z])?(?:[a-zA-Z]+)? (<\\B@![0-9]+\\b>)";
-  public static final  String CHANNEL_METION_REGEX= "(^[^0-9A-Za-z])([a-z]+)(?:[^a-zA-Z])?(?:[a-zA-Z]+)? (<\\B#![0-9]+\\b>)";
+  public static final  String CHANNEL_METION_REGEX= "(^[^0-9A-Za-z])([a-z]+)(?:[^a-zA-Z])?(?:[a-zA-Z]+)? (<\\B#[0-9]+\\b>)";
+  public static final  String ROLE_METION_REGEX= "(^[^0-9A-Za-z])([a-z]+)(?:[^a-zA-Z])?(?:[a-zA-Z]+)? (<\\B@&[0-9]+\\b>)";
+
 
 }
